dc57c32f76e559b7f9c234c39fa28940bc48a2e9
nm <maedan75@gmail.com>
2020-05-16 11:31:14 +0900

    fix

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const ORIGIN = "/ServiceWorker-test";
 
-// if (!navigator.serviceWorker) return;
+if (navigator.serviceWorker) {
+	// SWの登録を行う。登録の後でインストールされる。ここでparsed状態に入るってことか。
+	// register関数の第2引数でscopeを指定できる(scopeとなる範囲のルートを指定する)。
+	// デフォルトでは、register関数を実行するjsファイルと同じ階層(以下)になる
+	navigator.serviceWorker.register(ORIGIN + "/sw.js")
+		.then(function (reg) {
+			// 登録の成功
+			console.log("SWのScope: " + reg.scope);
 
-// SWの登録を行う。登録の後でインストールされる。ここでparsed状態に入るってことか。
-// register関数の第2引数でscopeを指定できる(scopeとなる範囲のルートを指定する)。
-// デフォルトでは、register関数を実行するjsファイルと同じ階層(以下)になる
-navigator.serviceWorker.register(ORIGIN + "/sw.js")
-	.then(function (reg) {
-		// 登録の成功
-		console.log("SWのScope: " + reg.scope);
+		}).catch(function (e) {
+			// 登録の失敗
+			console.log("SWの登録失敗:" + e);
+		});
 
-	}).catch(function (e) {
-		// 登録の失敗
-		console.log("SWの登録失敗:" + e);
-	});
+}
+
